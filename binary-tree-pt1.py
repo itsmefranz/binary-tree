c98166e69df7b953ca1ce7fdd68e46baf9579448
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-14 12:53:48 +0800

    defined build_tree method and added a numbers list and variable and a print function

--- binary-tree-pt1.py
+++ binary-tree-pt1.py
@@ -1,41 +1,52 @@
 class BinarySearchTreeNode:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-def add_child(self,data):
-    if data == self.data:
-        return
-    if data < self.data:
-        # add data in left subtree
+    def add_child(self, data):
+        if data == self.data:
+            return
+            
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        # this method returns a list of elements in a specific order
+        elements = []
+
+        # visit left tree
         if self.left:
-            self.left.add_child(data)
-        else:
-            self.left = BinarySearchTreeNode(data)
-    else:
-        # add data in right subtree
+            elements += self.left.in_order_traversal()
+
+        # visit base node
+        elements.append(self.data)
+
+        # visit right tree
         if self.right:
-            self.right.add_child(data)
-        else:
-            self.right = BinarySearchTreeNode(data)
+            elements += self.right.in_order_traversal()
 
-def in_order_traversal(self):
-    # this method returns a list of elements in a specific order
-    elements = []
+        return elements
 
-    # visit left tree
-    if self.left:
-        elements += self.left.in_order_traversal()
+    def build_tree(elements):
+        root = BinarySearchTreeNode(elements[0])
 
-    # visit base node
-    elements.append(self.data)
+        for i in range(1,len(elements)):
+            root.add_child(elements[i])
 
-    # visit right tree
-    if self.right:
-        elements += self.right.in_order_traversal()
+        return root
 
-    return elements
-
-if __name__ == '__main__':
-    numbers_tree = build_tree([17, 4, 1, 20, 9, 23, 18, 34])
+    if __name__ == '__main__':
+        numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+        numbers_tree = build_tree(numbers)
+        print(numbers_tree.in_order_traversal())
