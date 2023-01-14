46b6c8e0584efa6823dcbe7e6f7806ab3956f6eb
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-14 11:23:33 +0800

    completed the setlled conditions in the add child method

--- binary-tree-pt1.py
+++ binary-tree-pt1.py
@@ -9,5 +9,14 @@ def add_child(self,data):
         return
     if data < self.data:
         # add data in left subtree
+        if self.left:
+            self.left.add_child(data)
+        else:
+            self.left = BinarySearchTreeNode(data)
     else:
         # add data in right subtree
+        if self.right:
+            self.right.add_child(data)
+        else:
+            self.right = BinarySearchTreeNode(data)
+
