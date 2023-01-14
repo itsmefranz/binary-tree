062e87815a6056b0659d1f08a5ea67a6a3958204
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-14 11:11:55 +0800

    added a method and settled conditions as well as comments but still not finished

--- binary-tree-pt1.py
+++ binary-tree-pt1.py
@@ -3,3 +3,11 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+def add_child(self,data):
+    if data == self.data:
+        return
+    if data < self.data:
+        # add data in left subtree
+    else:
+        # add data in right subtree
