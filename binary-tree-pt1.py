2b6e98ecee6fa1d481e9e32be23d879df7942320
Daniella Francine P. Velasquez <daniellafrancine.velasquez@gmail.com>
2023-01-14 12:56:48 +0800

    fixed an error that wont allow my code to work, looks like i defined the build tree within the binarytree class

--- binary-tree-pt1.py
+++ binary-tree-pt1.py
@@ -38,15 +38,15 @@ class BinarySearchTreeNode:
 
         return elements
 
-    def build_tree(elements):
-        root = BinarySearchTreeNode(elements[0])
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
 
-        for i in range(1,len(elements)):
-            root.add_child(elements[i])
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
 
-        return root
+    return root
 
-    if __name__ == '__main__':
-        numbers = [17, 4, 1, 20, 9, 23, 18, 34]
-        numbers_tree = build_tree(numbers)
-        print(numbers_tree.in_order_traversal())
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
